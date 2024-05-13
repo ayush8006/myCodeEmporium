1a68e55320b62ed847c9cd38de6db2946d33fd61
ayush8006 <ayushagrawal8006@gmail.com>
2024-05-13 06:04:16 +0530

    Feat:Added routing structure for versioned api for problems

---
+++ src/routes/v1/problems.routes.js
@@ -0,0 +1,22 @@
+const express=require('express');
+
+const { ProblemController}=require('../../controllers')
+
+const problemRouter=express.Router();
+
+problemRouter.get('/ping',ProblemController.pingProblemController);
+
+problemRouter.get('/:id',ProblemController.getProblem);
+
+problemRouter.get('/',ProblemController.getProblems);
+
+problemRouter.post('/',ProblemController.addProblem);
+
+problemRouter.delete('/:id',ProblemController.deleteProblem);
+
+problemRouter.put('/',ProblemController.updateProblem);
+
+module.exports=problemRouter;
+
+
+
